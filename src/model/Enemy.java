fa8ff2b12d3210d4622201edd8e45364b3924041
Meryem <meryem19.t@gmail.com>
2026-05-20 01:25:49 +0200

    Project's week 4 update, day 1, half day 2

--- src/model/Enemy.java
+++ src/model/Enemy.java
@@ -14,15 +14,14 @@ public class Enemy {
     private int x;
     private int y; //coordinates
 
-    private int velocita = 2; //speed
+    private int velocita = 1; //speed
     private int puntiVita = 2; ///health points (2=1 heart)
 
     //to make the enemy move
     private Direzione direzione = Direzione.SUD;
-
     private final Random random = new Random();
-
     private int contatorePassi = 0;
+    
     //construct
     public Enemy(int xIniziale, int yIniziale) {
 
@@ -35,19 +34,42 @@ public class Enemy {
     public void setAlive(boolean alive) {
     	this.alive = alive;
     }
+    
+ 
     //the brain, it will be called by the game loop each 60 updates
     public void aggiorna() {
-
         contatorePassi++;
-
         if (contatorePassi > 60) {
-
-            scegliDirezioneCasuale();
-
-            contatorePassi = 0;
+        scegliDirezioneCasuale();
+        contatorePassi = 0;
         }
-      
+      //20/05
+        moveEnemy();
+    }
+    
+//20/05 
+    public void followPlayer(Player player) {
+    //x
+    if (this.x < player.getX()) {
+        this.x += velocita;
+        direzione = Direzione.EST;
+    } else if (this.x > player.getX()) {
+        this.x -= velocita;
+        direzione = Direzione.OVEST;
+    }
+
+    //y
+    if (this.y < player.getY()) {
+        this.y += velocita;
+        direzione = Direzione.SUD;
+    } else if (this.y > player.getY()) {
+        this.y -= velocita;
+        direzione = Direzione.NORD;
+    }
+}
+    
    //movement based on the preferred direction
+    public void moveEnemy() {
         switch (direzione) {
 
             case NORD:
@@ -68,9 +90,7 @@ public class Enemy {
     }
 
     private void scegliDirezioneCasuale() {
-
         int numero = random.nextInt(5);
-
         switch (numero) {
 
             case 0:
@@ -90,14 +110,21 @@ public class Enemy {
             	break;
         }
     }
+    
   //to understand if the Enemy made contact with the Player or an object
     public Rectangle getBounds() {
         return new Rectangle(x, y, 32, 32);
     }
+    
 //when the enemy loses health points
     public void subisciDanno(int danno) {
-        puntiVita -= danno;
+      puntiVita-= danno;
+      
+      if (puntiVita <=0) {
+    	  alive = false;
+      }
     }
+    
 //when enemy dead
     public boolean nemicoMorto() {
         return puntiVita <= 0;
--- src/model/GameState.java
+++ src/model/GameState.java
@@ -9,9 +9,13 @@ public class GameState {
     private Enemy enemy;
     private Enemy enemy2; //update 15/05
     private Level livelloAttuale;
+    
     private boolean gameOver = false;
     private boolean collision = false;
     private int score = 0;
+    
+    private int cooldownDamage = 0;
+    
     //construct
     public GameState() {
         // PLayer spawns in the middle of the screen
@@ -32,34 +36,62 @@ public class GameState {
 
     // the total brain
     public void update() {
-        if (gameOver) { 
-        	return; // so we stop when game over
-        }
+        if (gameOver) return; // so we stop when game over
+        
         
      // 15/05
         enemy.aggiorna(); // to move 1st enemy
         
-
-        // checks if player is alive or not
-        if (player.getPuntiVita() <= 0) {
-            gameOver = true;
+       //20/05
+        enemy2.followPlayer(player);
+       
+      if(cooldownDamage > 0) {
+    	  cooldownDamage--;
+      }
+       
+            //20/05 manage all of the collisions
+         checkCollision();
+         
         }
         
+      
+        private void checkCollision() {
+        	collision = false;
+       
         //colliding controls (MARIANA UPDATE 15/5)
         if (livelloAttuale.presenzaOstacolo(player.getX(), player.getY())) {
         	collision = true;
-        } else {
-        	collision = false;
-        }
-        
+        } 
+        //enemy 1
         if(player.getBounds().intersects(enemy.getBounds())) {
             collision = true;
-        }
+            if (cooldownDamage == 0) {
+            	loseLife();
+           }
+           //enemy 2 
         if(player.getBounds().intersects(enemy2.getBounds())) {
         	collision = true;
-        }
+        	if (cooldownDamage == 0) {
+            	loseLife();
+            	
+        	 }
+          }
+       }
     }
 
+
+    public void loseLife() {
+  
+    	player.setPuntiVita(player.getPuntiVita() - 1);
+    	
+    	cooldownDamage = 30;
+    	
+    	if (player.getPuntiVita() <= 0) {
+            gameOver = true;
+    	}
+    }
+    
+    
     // GETTERS 
     public Player getPlayer() { return player; }
     public Enemy getEnemy() {
